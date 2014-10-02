9015a1dca154eba0dee0423e39771b9d5423da0a
Anphamthanh <an.pham@gatech.edu>
2014-10-02 10:24:51 -0400

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :rememberable, :trackable, :validatable
+
+  validates_presence_of :username, :email, :password, :role
 end
