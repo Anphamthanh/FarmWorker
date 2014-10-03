e2b9738d5a37a5c28778e3cad40d80c6090717d2
Anphamthanh <an.pham@gatech.edu>
2014-10-03 12:48:36 -0400

    Deny faculty and student access to admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_admin
 
   def dashboard
   end
@@ -19,5 +19,12 @@ class AdminController < ApplicationController
 
   def admin_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :role)
+  end
+
+  def authenticate_admin
+    if current_user.role.downcase != 'admin'
+      flash[:alert] = "Access denied! Please log in as an administrator."
+      redirect_to root_path
+    end
   end
 end
