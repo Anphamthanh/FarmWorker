959e6b8d1c06c02455e79b49f0163ca40ea990a6
Anphamthanh <an.pham@gatech.edu>
2014-10-20 11:41:19 -0400

    Updated seed file for first admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(username: 'admin', first_name: 'First', last_name: 'Admin', password: 'password', email: 'admin@localhost.com', role: 1)
+User.create(username: 'admin', first_name: 'First', last_name: 'Admin', password: 'password', password_confirmation: 'password', email: 'admin@localhost.com', role: "Admin")
