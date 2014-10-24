bc7c7e76458d5632914978c719e64998997c18ad
Anphamthanh <an.pham@gatech.edu>
2014-10-24 13:08:30 -0400

    Patient creation works

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -6,7 +6,17 @@ class StudentController < ApplicationController
   end
 
   def new_patient
-    puts params
+    patient = Patient.new(patient_params)
+    patient.created_by = current_user.id
+    patient.patient_log = "Created by #{current_user.first_name} 
+          #{current_user.last_name} at #{Time.now}"
+    if patient.save
+      flash[:notice] = "The patient #{patient.first_name} #{patient.last_name} 
+          was successfully created"
+    else
+      flash[:alert] = "Failed to create patient: " + patient.errors.full_messages.join('. ')
+    end
+    redirect_to :student_dashboard
   end
 
   private
@@ -17,5 +27,9 @@ class StudentController < ApplicationController
       flash[:alert] = "Access denied! This page is for practitioners only."
       redirect_to root_path
     end
+  end
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :dob, :gender)
   end
 end
