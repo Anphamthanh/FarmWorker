19d98c9d280cda8a5bc0edfb2a5317f69ad15410
Anphamthanh <an.pham@gatech.edu>
2014-10-30 16:25:48 -0400

    Fixed db bug

--- app/models/patient_record.rb
+++ app/models/patient_record.rb
@@ -1,6 +1,7 @@
 class PatientRecord < ActiveRecord::Base
   belongs_to :patient
-  has_one :patient_demographics, :patient_vital, :patient_hearing, 
-          :patient_vision, :patient_physical, :patient_assessment
+  has_one :patient_demographics
+  # , :patient_vital, :patient_hearing, 
+  #         :patient_vision, :patient_physical, :patient_assessment
   validates :patient_id, presence: true
 end
