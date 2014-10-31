e966399471397a4590da07cc8f3755ebcd88e3ea
An Pham <an.pham@gatech.edu>
2014-10-30 21:36:06 -0400

    Added route and security for patient record

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,4 +1,7 @@
 class PatientController < ApplicationController
+
+  before_action :authenticate_practitioner
+
   def new
     patient = Patient.new(patient_params)
     patient.created_by = current_user.id
@@ -13,22 +16,45 @@ class PatientController < ApplicationController
   end
 
   def demographics
-    @patient = Patient.find(params[:id])
+    @patient = Patient.find_by_id(params[:id])
   end
 
   def view
-    @patient = Patient.find(params[:id])
-    record = @patient.patient_record
-    @demographics = record.patient_demographics
-    @vitals = record.patient_vital
-    @hearing = record.patient_hearing
-    @vision = record.patient_vision
-    @physical = record.patient_physical
-    @assessment = record.patient_assessment
+    @patient = Patient.find_by_id(params[:id])
+    if @patient.nil?
+      flash[:alert] = "Patient does not exist!" 
+      redirect_to dashboard
+    else 
+      record = @patient.patient_record
+      @demographics = record.patient_demographics
+      @vitals = record.patient_vital
+      @hearing = record.patient_hearing
+      @vision = record.patient_vision
+      @physical = record.patient_physical
+      @assessment = record.patient_assessment
+    end
   end
 
   private
   def patient_params
     params.require(:patient).permit(:first_name, :last_name, :dob, :gender)
+  end
+
+  def dashboard
+    if current_user.role.downcase == "student-bsn"
+      student_dashboard_path
+    elsif current_user.role.downcase == "faculty"
+      faculty_dashboard_path
+    else
+      root_path
+    end
+  end
+
+  def authenticate_practitioner    
+    authenticate_user!
+    if !['faculty', 'student-bsn', 'student-msn'].include?(current_user.role.downcase)
+      flash[:alert] = "Access denied! This page is for practitioners only."
+      redirect_to root_path
+    end
   end
 end
