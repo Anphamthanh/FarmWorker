2eca2116106c3c2a1190a4cc18d02c0f82d77306
An Pham <an.pham@gatech.edu>
2014-11-10 10:56:27 -0500

    Timeout user in 15 minutes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :timeoutable
 
   validates_presence_of :username, :email, :password, 
                         :role, :first_name, :last_name
