f39c32e0b7a4d9bbc9f1058f4e8c2d8df597b367
An Pham <an.pham@gatech.edu>
2014-11-10 11:21:06 -0500

    Removed patient log reference

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -5,7 +5,6 @@ class PatientController < ApplicationController
   def new
     patient = Patient.new(patient_params)
     patient.created_by = current_user.id
-    patient.patient_log = "Created by #{current_user.first_name} #{current_user.last_name} at #{Time.now}"
     if patient.save
       flash[:notice] = "The patient #{patient.first_name} #{patient.last_name} was successfully created"
       redirect_to :controller => "patient", :action => "input", :id => patient.id, :type => "demographics"
