7df8a0af1835e60895e4f10fd660ebd25ae0d8c9
An Pham <an.pham@gatech.edu>
2014-11-10 13:11:23 -0500

    Show description when parent permission is limited

--- db/migrate/20141022155526_create_patients.rb
+++ db/migrate/20141022155526_create_patients.rb
@@ -3,7 +3,7 @@ class CreatePatients < ActiveRecord::Migration
     create_table :patients do |t|
       t.string :first_name,           :null => false, :default => ""
       t.string :last_name,            :null => false, :default => ""
-      t.string :middle_name,          :null => false, :default => ""
+      t.string :middle_name
       t.string :gender,               :null => false
       t.date :dob,                    :null => false
       # parent_permission is saved in the form ["None", ""] or ["Complete", ""] or ["Limited", "<Explanation>"]
