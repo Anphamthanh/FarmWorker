f9d2aca17b3b368ce06954abd966a2613d933286
An Pham <an.pham@gatech.edu>
2014-11-11 09:59:14 -0500

    Ensure capitalization in names

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,7 @@ class Patient < ActiveRecord::Base
   validates_presence_of :parent_permission, if: :is_under_18?
   validates_presence_of :parent_permission_desc, if: [:is_limited?, :is_under_18?]
   has_one :patient_record
+
   def self.search(search)
     if search
       where(['first_name LIKE :s OR last_name LIKE :s OR id LIKE :s', :s => "%#{search}%"]).order('last_name ASC')
@@ -15,7 +16,20 @@ class Patient < ActiveRecord::Base
   def is_under_18?
     (Date.today - dob).to_i / 365 < 18
   end
+
   def is_limited?
     parent_permission.downcase == "limited"
+  end
+
+  def first_name=(s)
+    write_attribute(:first_name, s.to_s.capitalize)
+  end
+
+  def last_name=(s)
+    write_attribute(:last_name, s.to_s.capitalize)
+  end
+
+  def middle_name=(s)
+    write_attribute(:middle_name, s.to_s.capitalize)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :email, :password, 
                         :role, :first_name, :last_name
+
+
   def self.search(search)
     if search
       where(['first_name LIKE :s OR last_name LIKE :s OR username LIKE :s OR id LIKE :s', :s => "%#{search}%"]).order('last_name ASC')
@@ -13,4 +15,14 @@ class User < ActiveRecord::Base
       User.all.order('last_name ASC')
     end
   end
+
+  def first_name=(s)
+    write_attribute(:first_name, s.to_s.capitalize)
+  end
+
+  def last_name=(s)
+    write_attribute(:last_name, s.to_s.capitalize)
+  end
+
+  
 end
