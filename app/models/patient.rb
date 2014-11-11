7a849b85ddc021aa7b6219527974179cb7d2eb98
An Pham <an.pham@gatech.edu>
2014-11-11 10:35:07 -0500

    Test

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,10 +6,14 @@ class Patient < ActiveRecord::Base
   has_one :patient_record
 
   def self.search(search)
-    if search
-      where(['first_name LIKE :s OR last_name LIKE :s OR id LIKE :s', :s => "%#{search}"]).order('last_name ASC')
+    if !(search =~ /\A[-+]?\d*\.?\d+\z/).nil?
+      where(['id = :s', :s => "#{search}"])
     else
-      Patient.all.order('last_name ASC')
+      if search
+        where(['first_name LIKE :s OR last_name LIKE :s', :s => "%#{search}%"]).order('last_name ASC')
+      else
+        Patient.all.order('last_name ASC')
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   def self.search(search)
     if search
-      where(['first_name LIKE :s OR last_name LIKE :s OR username LIKE :s OR id LIKE :s', :s => "%#{search}"]).order('last_name ASC')
+      where(['first_name LIKE :s OR last_name LIKE :s OR username LIKE :s', :s => "%#{search}%"]).order('last_name ASC')
     else
       User.all.order('last_name ASC')
     end
