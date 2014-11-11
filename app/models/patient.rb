8e450d380d5aa0a8dc28d1785556bd7b5c87f206
An Pham <an.pham@gatech.edu>
2014-11-11 10:18:40 -0500

    Postgresl bug

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -7,7 +7,7 @@ class Patient < ActiveRecord::Base
 
   def self.search(search)
     if search
-      where(['first_name LIKE :s OR last_name LIKE :s OR id LIKE :s', :s => "%#{search}%"]).order('last_name ASC')
+      where(['first_name LIKE :s OR last_name LIKE :s OR id LIKE :s', :s => "%#{search}"]).order('last_name ASC')
     else
       Patient.all.order('last_name ASC')
     end
