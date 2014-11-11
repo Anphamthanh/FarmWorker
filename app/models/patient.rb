6402b685e027ca6b2e4b98ad5a5bb52168340f67
An Pham <an.pham@gatech.edu>
2014-11-10 19:04:55 -0500

    Added search by ID

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,7 +4,7 @@ class Patient < ActiveRecord::Base
   has_one :patient_record
   def self.search(search)
     if search
-      where(['first_name LIKE :s OR last_name LIKE :s', :s => "%#{search}"]).order('last_name ASC')
+      where(['first_name LIKE :s OR last_name LIKE :s OR id LIKE :s', :s => "%#{search}"]).order('last_name ASC')
     else
       Patient.all.order('last_name ASC')
     end
