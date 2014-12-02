d2aad3be2ef30cf661e839121c78ba2df626ec8f
An Pham <an.pham@gatech.edu>
2014-12-02 00:23:55 -0500

    Added tester to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(username: 'admin', first_name: 'First', last_name: 'Admin', password: 'password', password_confirmation: 'password', email: 'admin@localhost.com', role: "Admin")
 User.create(username: 'student', first_name: 'First', last_name: 'Student', password: 'password', password_confirmation: 'password', email: 'student@localhost.com', role: "Student-BSN")
+User.create(username: 'lellis4', first_name: 'Laura', last_name: 'Hilb', password: 'password', password_confirmation: 'password', email: 'lellis4@emory.edu', role: "Student-BSN")
+User.create(username: 'edownes', first_name: 'Elizabeth', last_name: 'Downes', password: 'password', password_confirmation: 'password', email: 'edownes@emory.edu', role: "Student-BSN")
+User.create(username: 'rkoontz', first_name: 'Rachel', last_name: 'Koontz', password: 'password', password_confirmation: 'password', email: 'rkoontz@emory.edu', role: "Student-BSN")
+User.create(username: 'micikas', first_name: 'Mary', last_name: 'Micikas', password: 'password', password_confirmation: 'password', email: 'mary.elizabeth.micikas@emory.edu', role: "Student-BSN")
