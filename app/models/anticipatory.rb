4f68f9f5e8e7908b34c95e1552a398b2a14a77c7
An Pham <an.pham@gatech.edu>
2014-12-03 00:12:33 -0500

    Cleaner code

--- app/models/anticipatory.rb
+++ app/models/anticipatory.rb
@@ -4,47 +4,60 @@ class Anticipatory < ActiveRecord::Base
   def get_list
     patient = Patient.find(patient_id)
     if !patient.is_under_3? and patient.is_under_6?
-      ["Health Promotion/Safety", "Developemental Stimulation", "Socialization", "Nutrition"]
+      {
+        "Health Promotion/Safety" => [
+          "Please choose one",
+          "Seat Belts – Place in back of vehicle/Airbag safety",
+          "Teach to answer phone/get help",
+          "Teach name, address, phone number",
+          "Secure matches, weapons",
+          "Teach to avoid 'bad touch'",
+          "Encourage active play",
+          "safe outdoor play areas",
+          "Street crossing",
+          "Dental flossing",
+          "Bike, water safety",
+          "Supervision at bath time",
+          "Out of home care",
+          "Lead Education",
+          "Important of up-to-date immunizations"
+          ],
+        "Developemental Stimulation" => [
+          "Please choose one",
+          "Riding Toys",
+          "Simple chores",
+          "Recognizes Coins",
+          "Colors",
+          "Scissors, clay, yarn",
+          "Begin self hygiene, grooming (5 yr)",
+          "Shoe tying (5 yr)",
+          "Negotiation skills"
+          ],
+        "Socialization" => [
+          "Please choose one",
+          "Respect for authority figures",
+          "'Bad' language",
+          "Expect loud boisterous behavior",
+          "Different developmental rates",
+          "Separation anxiety",
+          "School readiness/following rules",
+          "Sharing/respect for other's rights"
+          ],
+        "Nutrition" => [
+          "Please choose one",
+          "Review balanced diet/snacks",
+          "Food acceptance/good food habits",
+          "Limit salt, sugar, fat",
+          "Iron rich foods, Folic Acid/Building Blood",
+          "Choking on foods",
+          "Picky eater/food jags",
+          "WIC Topics: Reason certified",
+          "WIC Topics: Prescribe food package",
+          "WIC Topics: Using WIC foods"
+          ]  
+      }
     end
   end
-
-  def get_db_key(text)
-    KEY_LIST[text]
-  end
-
-  def get_details(key)
-    ANTICIPATORY[key]
-  end
-
-  ANTICIPATORY = {
-    "Health Promotion/Safety" => [
-      "Please choose one",
-      "Seat Belts – Place in back of vehicle/Airbag safety",
-      "Teach to answer phone/get help",
-      "Teach name, address, phone number",
-      "Secure matches, weapons",
-      "Teach to avoid 'bad touch'",
-      "Encourage active play",
-      "safe outdoor play areas",
-      "Street crossing",
-      "Dental flossing",
-      "Bike, water safety",
-      "Supervision at bath time",
-      "Out of home care",
-      "Lead Education",
-      "Important of up-to-date immunizations"
-      ],
-    "Developemental Stimulation" => [
-      ],
-    "Socialization" => [
-      ],
-    "Nutrition" => [
-      ]  
-  }
-
-  KEY_LIST = {
-    "Health Promotion/Safety" => "health_promotion"
-  }
 
   
 end
