ebaefd8d0518d35d0db97939619b825cbe3fbad7
An Pham <an.pham@gatech.edu>
2014-12-03 10:19:42 -0500

    Support student MSN dashboard and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include ApplicationHelper
 
   def after_sign_in_path_for(resource)
-    if resource.role.downcase == "admin"
-      admin_dashboard_path
-    elsif resource.role.downcase == "faculty"
-      faculty_dashboard_path
-    elsif ["student-bsn","student-msn"].include? resource.role.downcase
-      student_dashboard_path
-    else
-      root_path
-    end
+    dashboard
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,18 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def dashboard
+    if current_user
+      if current_user.role.downcase == "admin"
+        admin_dashboard_path
+      elsif ["student-bsn", "student-msn"].include? current_user.role.downcase
+        student_dashboard_path
+      elsif current_user.role.downcase == "faculty"
+        faculty_dashboard_path
+      end
+    else
+      root_path
+    end
+  end
 end
